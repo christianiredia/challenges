178019d87d46dac2f5123483f010f24a0312fc50
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2023-01-18 01:05:53 -0600

    if else statement to evaulate if population is above a given number

---
+++ ifElseStatement/index.js
@@ -0,0 +1,7 @@
+const population = 331000000;
+
+if (population > 33000000) {
+  console.log("Portugal population is above average.");
+} else {
+  console.log("Portugal population is 22 million bellow average");
+}
