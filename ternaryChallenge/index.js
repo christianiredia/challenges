fc818b2a40d067ec492c30d4989a1dc0502e6e09
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2023-01-19 01:36:03 -0600

    Created Ternary operator to get value for tip variable which was used to find total value

---
+++ ternaryChallenge/index.js
@@ -0,0 +1,15 @@
+let value = 430;
+let tip = value >= 50 && value <= 300 ? value * 0.15 : value * 0.2;
+let totalValue = value + tip;
+
+console.log(
+  `The Bill was $${value}, the tip was $${tip}, and the total value as ${totalValue}`
+);
+
+console.log(
+  `The Bill was $${value}, the tip was $${tip}, and the total value as ${totalValue}`
+);
+
+console.log(
+  `The Bill was $${value}, the tip was $${tip}, and the total value as ${totalValue}`
+);
