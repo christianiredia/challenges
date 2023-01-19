136f375022970e81c75be08214653451cd270f28
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2023-01-19 17:13:20 -0600

    Calculated tips, then the total after tips, displayed via arrays

---
+++ JAVASCRIPT/arrayChallenge/index.js
@@ -0,0 +1,17 @@
+const calcTip = (value) => {
+  if (value >= 50 && value <= 300) {
+    return value * 0.15;
+  } else {
+    return value * 0.2;
+  }
+};
+
+const bills = [125, 555, 44];
+console.log("Bills: " + bills);
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log("Tips: " + tips);
+
+//Bonus:
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log("Total: " + total);
