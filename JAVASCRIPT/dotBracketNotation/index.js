427ae4d804c3d465421826f17ec0b9917ca6e139
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2023-01-19 18:34:33 -0600

    Created object then logged sentence destructing the object, then increased decreased population by two using dot and bracket notation

---
+++ JAVASCRIPT/dotBracketNotation/index.js
@@ -0,0 +1,16 @@
+const country = {
+  name: "Finland",
+  population: 6,
+  neighbours: ["Norway", "Sweeden", "Russia"],
+  capital: "Helsinki",
+};
+
+console.log(
+  `${country.name} has ${country.population} million finnish-speaking people, ${country.neighbours.length} neighbouring countries and a capital called ${country.capital}.`
+);
+
+country.population += 2;
+console.log(country.population);
+
+country.population -= 2;
+console.log(country.population);
