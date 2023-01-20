1ab300dc42c7463c66c7c6548b5794a2e58527f5
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2023-01-19 22:01:31 -0600

    Created nested arrays, looped through the arrays to get the individual values

---
+++ loopingBackwardsLoopsInLoops/index.js
@@ -0,0 +1,11 @@
+const listOfNeighbours = [
+  ["Canada", "Mexico"],
+  ["Spain"],
+  ["Norway", "Sweden", "Russia"],
+];
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+  for (let x = 0; x < listOfNeighbours[i].length; x++) {
+    console.log(`Neighbour: ${listOfNeighbours[i][x]}`);
+  }
+}
